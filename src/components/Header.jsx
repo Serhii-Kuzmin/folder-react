5713394f6030e66743036cb6fe94c729a0a6fe17
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-02 15:29:17 +0100

    &&

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 export const Header = ({ title }) => {
+  const isLogin = true;
   // console.log(props)
   return (
     <header>
       <h1>{title}</h1>
       <hr />
+      {/* {isLogin && <h2>Welcome back</h2>} */}
+      {isLogin ? <h2>Online</h2> : <h2>Offline</h2>}
     </header>
   );
 };
